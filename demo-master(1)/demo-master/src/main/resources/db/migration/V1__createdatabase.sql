4cd51022cd353756f6770f27af151b01dac74b37
Pau Llaurado <77300359+PauLlaurado@users.noreply.github.com>
2022-03-01 18:12:46 +0100

    Update V1__createdatabase.sql

--- demo-master(1)/demo-master/src/main/resources/db/migration/V1__createdatabase.sql
+++ demo-master(1)/demo-master/src/main/resources/db/migration/V1__createdatabase.sql
@@ -65,12 +65,7 @@ create TABLE pedidos_productes(
 
 
 
-CREATE TABLE usser (
-  userid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-  username varchar(24) NOT NULL UNIQUE,
-  password varchar(255) NOT NULL,
-  role varchar(10),
-  enabled boolean DEFAULT true);
+
 
 
 
