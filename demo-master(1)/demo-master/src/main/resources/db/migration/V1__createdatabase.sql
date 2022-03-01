ff23ad7baed747cb60beeeb0574de1f906504064
Pau Llaurado <77300359+PauLlaurado@users.noreply.github.com>
2022-03-01 18:11:23 +0100

    Update V1__createdatabase.sql

--- demo-master(1)/demo-master/src/main/resources/db/migration/V1__createdatabase.sql
+++ demo-master(1)/demo-master/src/main/resources/db/migration/V1__createdatabase.sql
@@ -1,55 +1,3 @@
-CREATE TABLE movie (
-    movieid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    title text,
-    synopsis text,
-    imageurl text);
-
-CREATE TABLE actor (
-    actorid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    name text,
-    imageurl text);
-
-CREATE TABLE genre (
-    genreid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    label text);
-
-CREATE TABLE movie_actor (
-    movieid uuid REFERENCES movie(movieid) ON DELETE CASCADE,
-    actorid uuid REFERENCES actor(actorid) ON DELETE CASCADE,
-    PRIMARY KEY (movieid, actorid));
-
-CREATE TABLE movie_genre (
-    movieid uuid REFERENCES movie(movieid) ON DELETE CASCADE,
-    genreid uuid REFERENCES genre(genreid) ON DELETE CASCADE,
-    PRIMARY KEY (movieid, genreid));
-
-CREATE TABLE file (
-    fileid UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    contenttype TEXT,
-    data bytea);
-
-CREATE TABLE usser (
-  userid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-  username varchar(24) NOT NULL UNIQUE,
-  password varchar(255) NOT NULL,
-  role varchar(10),
-  enabled boolean DEFAULT true);
-
-CREATE TABLE favorite (
-    userid uuid REFERENCES usser(userid) ON DELETE CASCADE,
-    pedidosid uuid REFERENCES pedidos(pedidosuuid) ON DELETE CASCADE,
-    PRIMARY KEY(userid, pedidosid)
-);
-
-insert into favorite values((select userid from usser where username='user'),(select pedidosuuid from pedidos where entregat ='f'));
-insert into favorite values((select userid from usser where username='user'),(select pedidosuuid from pedidos where data ='2020-08-12'));
-
-create TABLE pedidos_productes(
-    pedidosid uuid REFERENCES pedidos(pedidosuuid) ON DELETE CASCADE,
-    producteid uuid REFERENCES productes(producteid) ON DELETE CASCADE,
-    PRIMARY KEY (pedidosid,producteid));
-
-    insert into pedidos_productes values((select pedidosuuid from pedidos where data='2020-12-12'),(select producteid from productes where cant =10));
 
 
 
@@ -63,11 +11,7 @@ create TABLE pedidos_productes(
     groups int,
     enabled boolean DEFAULT false
   );
-  INSERT INTO usser (username, password, role, groups) VALUES ('user', crypt('pass', gen_salt('bf')), true,0);
-  INSERT INTO usser (username, password, role, groups) VALUES ('user2', crypt('1324', gen_salt('bf')), true,0);
-  INSERT INTO usser (username, password, role, groups) VALUES ('user3', crypt('1234', gen_salt('bf')), false ,1);
-  INSERT INTO usser (username, password, role, groups) VALUES ('user4', crypt('1234', gen_salt('bf')), false ,1);
---------
+ 
   Create TABLE missatges(
     missatgesid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     missatges varchar(255) NOT NULL,
@@ -75,85 +19,58 @@ create TABLE pedidos_productes(
     iduserenviat uuid REFERENCES usser(userid) ON DELETE CASCADE,
     iduserremitent uuid REFERENCES usser(userid) ON DELETE CASCADE
   );
------------
-  Create TABLE missatges(
-    missatgesid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    missatges varchar(255) NOT NULL,
-    traduccio varchar(255),
-  );
-  insert into missatges(missatges, iduserenviat,iduserremitent)values('hola com estas',(select userid from usser where username='user'),(select userid from usser where username='user3'));
-
-  create table missatgesremitent(
-
-   userid uuid REFERENCES usser(userid) ON DELETE CASCADE,
-   misatgesid uuid REFERENCES missatges(missatgesid) ON DELETE CASCADE,
-   PRIMARY KEY (userid, misatgesid));
-  );
-
-  insert into missatgesremitent values((select userid from usser where username='user'),(select missatgesid from missatges where missatges='hola com estas'));
 
 
-  create table missatgesdesti(
-   misatgesid uuid REFERENCES missatges(missatgesid) ON DELETE CASCADE,
-   userid uuid REFERENCES usser(userid) ON DELETE CASCADE,
-   PRIMARY KEY (userid, misatgesid));
-  );
-
-  insert into missatgesdesti values((select missatgesid from missatges where missatges='hola com estas'),(select userid from usser where username='user3'));
 
   Create TABLE productes(
     producteid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     nom varchar,
     cant int
   );
-  insert into productes(nom,cant) values('tuerca',10);
   create TABLE pedidos(
     pedidosuuid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     data varchar,
     entregat boolean  DEFAULT false
   );
-INSERT INTO pedidos(data,entregat) values('2020-08-12',false);
+  
+  
+CREATE TABLE favorite (
+    userid uuid REFERENCES usser(userid) ON DELETE CASCADE,
+    pedidosid uuid REFERENCES pedidos(pedidosuuid) ON DELETE CASCADE,
+    PRIMARY KEY(userid, pedidosid)
+);
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-INSERT INTO usser (username, password) VALUES ('user', crypt('pass', gen_salt('bf')));
-INSERT INTO usser (username, password) VALUES ('pepe', crypt('pass', gen_salt('bf')));
+
+create TABLE pedidos_productes(
+    pedidosid uuid REFERENCES pedidos(pedidosuuid) ON DELETE CASCADE,
+    producteid uuid REFERENCES productes(producteid) ON DELETE CASCADE,
+    PRIMARY KEY (pedidosid,producteid));
 
 
-INSERT INTO movie(title, synopsis, imageurl) VALUES
-    ('Movie One','This is the One Movie','movie1.jpg'),
-    ('Movie Two','The Two Movie is the next','movie2.jpg'),
-    ('Movie Three','The Trilogy','movie3.jpg'),
-    ('Movie Four','Four movies is too much','movie4.jpg');
 
-INSERT INTO actor(name, imageurl) VALUES
-    ('Actor One','actor1.jpg'),
-    ('Actor Two','actor2.jpg'),
-    ('Actor Three','actor3.jpg'),
-    ('Actor Four','actor4.jpg'),
-    ('Actor Five','actor5.jpg');
+  
+  INSERT INTO usser (username, password, role, groups) VALUES ('user', crypt('pass', gen_salt('bf')), true,0);
+  INSERT INTO usser (username, password, role, groups) VALUES ('user2', crypt('1324', gen_salt('bf')), true,0);
+  INSERT INTO usser (username, password, role, groups) VALUES ('user3', crypt('1234', gen_salt('bf')), false ,1);
+  INSERT INTO usser (username, password, role, groups) VALUES ('user4', crypt('1234', gen_salt('bf')), false ,1);
+  
+  INSERT INTO pedidos(data,entregat) values('2020-08-12',false);
+  insert into productes(nom,cant) values('tuerca',10);
 
-INSERT INTO genre(label) VALUES
-    ('Genre One'),
-    ('Genre Two'),
-    ('Genre Three');
+  insert into pedidos_productes values((select pedidosuuid from pedidos where data='2020-12-12'),(select producteid from productes where cant =10));
 
-INSERT INTO movie_actor VALUES
-    ((SELECT movieid FROM movie WHERE title='Movie One'),(SELECT actorid FROM actor WHERE name='Actor One')),
-    ((SELECT movieid FROM movie WHERE title='Movie One'),(SELECT actorid FROM actor WHERE name='Actor Two')),
-    ((SELECT movieid FROM movie WHERE title='Movie Two'),(SELECT actorid FROM actor WHERE name='Actor Three')),
-    ((SELECT movieid FROM movie WHERE title='Movie Two'),(SELECT actorid FROM actor WHERE name='Actor Four')),
-    ((SELECT movieid FROM movie WHERE title='Movie Three'),(SELECT actorid FROM actor WHERE name='Actor Four')),
-    ((SELECT movieid FROM movie WHERE title='Movie Three'),(SELECT actorid FROM actor WHERE name='Actor Five')),
-    ((SELECT movieid FROM movie WHERE title='Movie Four'),(SELECT actorid FROM actor WHERE name='Actor One')),
-    ((SELECT movieid FROM movie WHERE title='Movie Four'),(SELECT actorid FROM actor WHERE name='Actor Four'));
+  
+  insert into favorite values((select userid from usser where username='user'),(select pedidosuuid from pedidos where entregat ='f'));
+  insert into favorite values((select userid from usser where username='user'),(select pedidosuuid from pedidos where data ='2020-08-12'));
 
-INSERT INTO movie_genre VALUES
-    ((SELECT movieid FROM movie WHERE title='Movie One'),(SELECT genreid FROM genre WHERE label='Genre One')),
-    ((SELECT movieid FROM movie WHERE title='Movie One'),(SELECT genreid FROM genre WHERE label='Genre Two')),
-    ((SELECT movieid FROM movie WHERE title='Movie Two'),(SELECT genreid FROM genre WHERE label='Genre One')),
-    ((SELECT movieid FROM movie WHERE title='Movie Three'),(SELECT genreid FROM genre WHERE label='Genre One')),
-    ((SELECT movieid FROM movie WHERE title='Movie Three'),(SELECT genreid FROM genre WHERE label='Genre Two')),
-    ((SELECT movieid FROM movie WHERE title='Movie Three'),(SELECT genreid FROM genre WHERE label='Genre Three'));
 
-INSERT INTO favorite VALUES
-    ((SELECT userid FROM usser WHERE username = 'user'),(SELECT movieid FROM movie WHERE title='Movie One'));
+
+CREATE TABLE usser (
+  userid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+  username varchar(24) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  role varchar(10),
+  enabled boolean DEFAULT true);
+
+
+
